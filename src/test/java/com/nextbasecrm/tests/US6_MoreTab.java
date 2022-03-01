4c3f9ebe4bc68e550d3232f309c5e36b2517d946
Mullajanov <seanmullajanov@gmail.com>
2022-02-28 20:26:36 -0800

    @Test with hr37 credentials done!

--- src/test/java/com/nextbasecrm/tests/US6_MoreTab.java
+++ src/test/java/com/nextbasecrm/tests/US6_MoreTab.java
@@ -127,4 +127,53 @@ public class US6_MoreTab {
 
     }
 
+    @Test //Test with hr37 credentials
+    public void nextBaseCrm_test3_hr37() {
+        //Enter user credentials  as hr37 user and click log in button
+        WebElement hrUserLogin = driver.findElement(By.xpath("//input[@placeholder='Login']"));
+        hrUserLogin.sendKeys("hr37@cydeo.com");
+
+        //Enter password UserUser
+        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        password.sendKeys("UserUser");
+
+        // Navigate to logInButton and click
+        WebElement logInButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        logInButton.click();
+
+        // Navigate to MoreTab and click
+        WebElement MoreTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-link-text']"));
+        MoreTab.click();
+
+        WebElement moreTabContains = driver.findElement(By.className("menu-popup-items"));
+        System.out.println("moreTabContains.getText() = " + moreTabContains.getText());
+
+        //Four options should be displayed under the MORE tab:
+        //File
+        //Appreciation
+        //Announcement
+        //Workflow
+     /*   if (moreTabContains.getText().contains("File\n" +
+                "Appreciation\n" +
+                "Announcement\n" +
+                "Workflow")){
+            System.out.println("More tab contains all options, verification Passed!");
+        }else {
+            System.out.println("More Tab contains all options, verification Failed!");
+        }
+
+      */
+        String expectedResult = "File\n" +
+                "Appreciation\n" +
+                "Announcement\n" +
+                "Workflow";
+
+        String actualResult = moreTabContains.getText();
+
+        Assert.assertEquals(actualResult, expectedResult, "Verification Failed");
+
+        driver.close();
+    }
+
+
 }
