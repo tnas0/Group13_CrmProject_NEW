f82263e34cb0296424a91cb4fe39b90966b110e8
Mullajanov <seanmullajanov@gmail.com>
2022-02-28 20:17:23 -0800

    @Before setup method created

--- src/test/java/com/nextbasecrm/tests/US6_MoreTab.java
+++ src/test/java/com/nextbasecrm/tests/US6_MoreTab.java
@@ -1,5 +1,30 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.ConfigurationReader;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class US6_MoreTab {
+
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //User navigate to Authorization page https://login2.nextbasecrm.com/
+        //driver.get("https://login2.nextbasecrm.com/");
+        driver.get(ConfigurationReader.getProperty("env"));
+
+    }
 
 }
