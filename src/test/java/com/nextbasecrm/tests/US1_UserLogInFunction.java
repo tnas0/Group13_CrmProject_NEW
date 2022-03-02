84374a99e667055eafe007931651cc3630e1fc99
LOUISALLENZHU <93153973+LOUISALLENZHU@users.noreply.github.com>
2022-03-01 20:32:13 -0800

    Code finished

--- src/test/java/com/nextbasecrm/tests/US1_UserLogInFunction.java
+++ src/test/java/com/nextbasecrm/tests/US1_UserLogInFunction.java
@@ -8,12 +8,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.lang.module.Configuration;
 import java.util.concurrent.TimeUnit;
 
 public class US1_UserLogInFunction {
@@ -99,6 +96,7 @@ public class US1_UserLogInFunction {
         }
     }*/
     WebDriver driver;
+    //just write anything
 
     @BeforeMethod
     public void setUp() {
